5da7a30fa74d00cc19df0c9bc38080e4eb4d9a10
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-02 11:33:50 +0200

    sélectionner une base de données

---
+++ clients.sql
@@ -0,0 +1,18 @@
+
+-- Authentification/connexion 
+>mysql -u root -- si l'utilisateur n'a pas de mdp 
+>mysql -u root -p -- si l'utilisateur possède un mdp 
+
+-- 1. Lister toutes les bases de données 
+
+SHOW DATABASES;
+
+-- 2. création de la base de données 
+
+CREATE DATABASE poe_ventes;
+
+-- 3. utiliser/sélectionner une base de données 
+-- USE <nom base_de_données>
+USE poe_ventes;
+
+-- 4. création de la table de données 
