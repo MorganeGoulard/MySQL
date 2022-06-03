5dcba4e047faddc307c4d357638b1679ce00fb44
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-03 11:03:05 +0200

    correction tp + where <colonne> in + order by

--- sales.sql
+++ sales.sql
@@ -9,8 +9,8 @@ USE clients;
 CREATE TABLE telephones(ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR (50) NOT NULL, 
 manufacturer VARCHAR (50) NOT NULL, 
-price INTEGER NOT NULL, 
-units_solds INTEGER
+price FLOAT NOT NULL, 
+units_solds FLOAT NOT NULL,
 );
 DESC telephones;
 
@@ -25,12 +25,28 @@ values ("N1280","Nokia",199,1925),("Iphone 4","Apple",399,9436),
 SELECT * FROM telephones;
 
 -- 5. Lister tous les téléphones (name,manufacturer) dont le prix>200€
-select name, manufacturer from telephones where price>200;
+select name, manufacturer from telephones where price>=200;
 
 --6. Lister les téléphones (name,manufacturer) dont prix varie entre 150 et 200€
-select name, manufacturer from telephones where price>150 and price<200;
+select name, manufacturer from telephones where price>=150 and price<=200;
+
+-- alternative : 
+SELECT name,manufacturer,price FROM telephones WHERE price BETWEEN 150 AND 200;
 
 --7. Lister les téléphones de marquer samsung et Apple 
-select * from telephones where manufacturer="Samsung" or "Apple";
+select * from telephones where manufacturer="Samsung" or manufacturer="Apple";
+
+-- autre façon de faire : 
+SELECT * FROM telephones WHERE manufacturer IN("Apple","Samsung"); 
+
+-- Afficher dans l'ordre croissance des prix
+SELECT name,manufacturer,price FROM telephones ORDER BY price ASC;
+-- pour prix décroissant : price DESC 
+
+-- Plusieurs filtres avec ordre 
+SELECT name,manufacturer,price FROM telephones ORDER BY price DESC, manufacturer DESC;
+
+
+
 
 
